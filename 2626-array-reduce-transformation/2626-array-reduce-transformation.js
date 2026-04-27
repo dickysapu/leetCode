60f1653c07c9f676fb23d9aab4ca945a1353be5e
dicky saputra <76934476+dickysapu@users.noreply.github.com>
2026-04-27 16:22:47 +0700

    Time: 34 ms (96.76%), Space: 52.2 MB (99.55%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -1,9 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {Function} fn
- * @param {number} init
- * @return {number}
- */
 var reduce = function(nums, fn, init) {
     let accum = init;
     for (let i = 0; i < nums.length; i++){
